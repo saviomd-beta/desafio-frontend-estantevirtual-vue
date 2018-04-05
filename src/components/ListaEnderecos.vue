88a87276a052843355325b904591cea44f977702
Sávio Mendes <saviomd@gmail.com>
2018-04-05 18:39:54 -0300

    Inclusão de componente de tabela de endereços cadastrados

--- src/components/ListaEnderecos.vue
+++ src/components/ListaEnderecos.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
-  div
+  div.mb-3
     .row
       .col
         h1.h3= 'Lista de endereços'
       .col-auto
         router-link.btn.btn-primary.btn-sm(to='/cadastro-endereco')= 'Adicionar'
+    tabela-enderecos(
+      v-bind:listaEnderecos='listaEnderecos'
+    )
 </template>
 
 <script>
+import TabelaEnderecos from '@/components/TabelaEnderecos';
+
 export default {
   name: 'ListaEnderecos',
+  props: ['listaEnderecos'],
+  components: {
+    'tabela-enderecos': TabelaEnderecos,
+  },
 };
 </script>
---
+++ src/components/TabelaEnderecos.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .table-responsive(v-if="listaEnderecos.length")
+    .table.table-striped
+      thead
+        tr
+          th= 'CEP'
+          th= 'Logradouro'
+          th= 'Complemento'
+          th= 'Bairro'
+          th= 'Localidade'
+          th= 'UF'
+      tbody
+        tr(v-for='endereco in listaEnderecos')
+          td= '{{endereco.cep}}'
+          td= '{{endereco.logradouro}}'
+          td= '{{endereco.complemento}}'
+          td= '{{endereco.bairro}}'
+          td= '{{endereco.localidade}}'
+          td= '{{endereco.uf}}'
+  .text-center(v-else)= 'Nenhum endereço cadastrado'
+</template>
+
+<script>
+export default {
+  name: 'TabelaEnderecos',
+  props: ['listaEnderecos'],
+};
+</script>
