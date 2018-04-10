927d2037217b78fb47594a40eeabd10c78c61b4b
Sávio Mendes <saviomd@gmail.com>
2018-04-10 16:34:55 -0300

    Inclusão de teste p/ tabela de endereços

---
+++ test/unit/specs/TabelaEnderecos.spec.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import TabelaEnderecos from '@/components/TabelaEnderecos';
+
+describe('TabelaEnderecos.vue', () => {
+  it('tabela deve ter 2 endereços exibidos', () => {
+    const propsData = {
+      listaEnderecos: [
+        {
+          cep: 'lorem1',
+          logradouro: 'lorem1',
+          complemento: 'lorem1',
+          bairro: 'lorem1',
+          localidade: 'lorem1',
+          uf: 'lorem1',
+        },
+        {
+          cep: 'lorem2',
+          logradouro: 'lorem2',
+          complemento: 'lorem2',
+          bairro: 'lorem2',
+          localidade: 'lorem2',
+          uf: 'lorem2',
+        },
+      ],
+    };
+    const Constructor = Vue.extend(TabelaEnderecos);
+    const vm = new Constructor({ propsData: propsData }).$mount();
+    expect(vm.$el.querySelectorAll('tbody > tr').length)
+      .toEqual(2);
+  });
+});
